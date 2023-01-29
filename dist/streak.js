1a07dfa2ba88f5d4b0cb4dcf470a64dd1eeb9c17
Felix Milgo <felixmilgo21@gmail.com>
2023-01-29 14:54:33 +0300

    Initial commit

---
+++ dist/streak.js
@@ -0,0 +1,5 @@
+"use strict";
+const addIcon = document.getElementById('add-icon');
+const welcomePage = document.querySelector(".welcome-page");
+const addPage = document.querySelector(".add-task");
+const bottomPage = document.querySelector(".bottom-page");
---
+++ src/streak.ts
@@ -0,0 +1,6 @@
+const addIcon = document.getElementById('add-icon') as HTMLButtonElement;
+const welcomePage = document.querySelector(".welcome-page") as HTMLDivElement;
+const addPage = document.querySelector(".add-task") as HTMLDivElement;
+const bottomPage = document.querySelector(".bottom-page") as HTMLDivElement;
+
+
