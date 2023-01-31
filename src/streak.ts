6ab782343fba51ba86ecf11424e9feae0ea82550
Felix Milgo <felixmilgo21@gmail.com>
2023-01-31 17:41:33 +0300

    More updates...

--- src/streak.ts
+++ src/streak.ts
@@ -1,6 +1,10 @@
 const addIcon = document.getElementById('add-icon') as HTMLButtonElement;
 const welcomePage = document.querySelector(".welcome-page") as HTMLDivElement;
 const addPage = document.querySelector(".add-task") as HTMLDivElement;
-const bottomPage = document.querySelector(".bottom-page") as HTMLDivElement;
+const bottomPage = document.querySelector(".delete-btn") as HTMLButtonElement;
+const closePopUp =  document.querySelector(".close-btn") as HTMLButtonElement;
 
-
+addIcon.addEventListener('Click',()=>{
+    welcomePage.style.display = 'none';
+    addPage.style.display = 'block';
+})
